853886c2508d843c0d8d5986ddca379a65ab59dd
Alan Lai <alanlai22@outlook.com>
2026-03-25 12:38:54 +0800

    add remaining serializers

--- BTAPI/serializer.py
+++ BTAPI/serializer.py
@@ -5,3 +5,23 @@ class TesterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tester
         fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'displayName', 'description', 'currentVersion', 'isActiveBeta']
+
+class ProductOwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductOwner
+        fields = ['id', 'fullName', 'email', 'username', 'isActive', 'productId']
+
+class DeveloperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Developer
+        fields = ['id', 'productId', 'fullName', 'email', 'username', 'isActive']
+
+class DefectReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DefectReport
+        fields = '__all__'
