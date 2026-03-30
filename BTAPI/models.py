97df815b5df55e8867f8abc6e55cad1497539aa7
Alan Lai <alanlai22@outlook.com>
2026-03-30 16:53:20 +0800

    modify fields to align with demo requirements

--- BTAPI/models.py
+++ BTAPI/models.py
@@ -2,42 +2,42 @@ from django.db import models
 
 class Tester(models.Model):
     id = models.CharField(primary_key=True)
+    email = models.EmailField(null=True, blank=True, unique=True)
+
+    def __str__(self):
+        return self.id
+    
+class ProductOwner(models.Model):
+    id = models.CharField(primary_key=True)
+    fullName = models.CharField()
     email = models.EmailField(null=True, blank=True)
+    username = models.CharField()
+    isActive = models.BooleanField()
+
+    def __str__(self):
+        return self.id
+    
+class Developer(models.Model):
+    id = models.CharField(primary_key=True)
+    fullName = models.CharField()
+    email = models.EmailField(null=True, blank=True)
+    username = models.CharField()
+    isActive = models.BooleanField()
 
     def __str__(self):
         return self.id
     
 class Product(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.CharField(primary_key=True)
     displayName = models.CharField()
     description = models.CharField()
     currentVersion = models.CharField()
     isActiveBeta = models.BooleanField()
+    ownerId = models.ForeignKey(ProductOwner, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True)
+    devId = models.ForeignKey(Developer, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True)
 
     def __str__(self):
-        return self.displayName
-    
-class ProductOwner(models.Model):
-    id = models.IntegerField(primary_key=True)
-    fullName = models.CharField()
-    email = models.EmailField(null=True, blank=True)
-    username = models.CharField()
-    isActive = models.BooleanField()
-    productId = models.ForeignKey(Product, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True)
-
-    def __str__(self):
-        return self.username
-    
-class Developer(models.Model):
-    id = models.IntegerField(primary_key=True)
-    productId = models.ForeignKey(Product, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True)
-    fullName = models.CharField()
-    email = models.EmailField(null=True, blank=True)
-    username = models.CharField()
-    isActive = models.BooleanField()
-
-    def __str__(self):
-        return self.username
+        return self.id
 
 class DefectReport(models.Model):
     class Status(models.TextChoices):
@@ -57,21 +57,22 @@ class DefectReport(models.Model):
         HIGH = 3, 'High'
         CRITICAL = 4, 'Critical'
 
-    id = models.IntegerField(primary_key=True)
-    testerId = models.ForeignKey(Tester, on_delete=models.SET_DEFAULT, default=None)
+    id = models.CharField(primary_key=True)
+    productId = models.ForeignKey(Product, on_delete=models.SET_DEFAULT, default=None)
+    productVersion = models.CharField()
     title = models.CharField()
     description = models.CharField()
     reproductionSteps = models.CharField()
-    evaluatedById = models.ForeignKey(ProductOwner, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True)
+    testerId = models.ForeignKey(Tester, on_delete=models.SET_DEFAULT, default=None)
+    testerEmail = models.EmailField(null=True, blank=True)
+    submittedAt = models.DateTimeField(auto_now_add=True)
     status = models.CharField(
         choices=Status.choices,
         default=Status.NEW,
     )
-    severity = models.IntegerField(choices=Severity.choices, default=Severity.LOW)
-    priority = models.IntegerField(choices=Priority.choices, default=Priority.LOW)
-    submittedAt = models.DateTimeField(auto_now_add=True)
-    testerEmail = models.EmailField(null=True, blank=True)
-    productId = models.ForeignKey(Product, on_delete=models.SET_DEFAULT, default=None)
+    severity = models.IntegerField(choices=Severity.choices, null=True, blank=True)
+    priority = models.IntegerField(choices=Priority.choices, null=True, blank=True)
+    evaluatedById = models.ForeignKey(ProductOwner, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True)
     assignedToId = models.ForeignKey(Developer, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True)
 
     def __str__(self):
--- BTAPI/urls.py
+++ BTAPI/urls.py
@@ -4,11 +4,11 @@ from .views import *
 urlpatterns = [
     path('defect/', post_new_report, name='post_new_report'),
     path('defects/<str:status>/', get_reports, name='get_reports'),
-    path('defects/Assigned/dev=<int:id>/', get_assigned_defects, name='get_assigned_defects'),
-    path('defect/<int:id>/', get_full_report, name='get_full_report'),
-    path('defects/<int:id>/<str:new_status>/dev=<int:dev_id>/', patch_update_report, name='patch_update_report'),
-    path('defects/<int:id>/<str:new_status>/dev=<int:dev_id>/severity=<int:new_severity>/', patch_update_report, name='patch_update_report'),
-    path('defects/<int:id>/<str:new_status>/dev=<int:dev_id>/priority=<int:new_priority>/', patch_update_report, name='patch_update_report'),
-    path('defects/<int:id>/<str:new_status>/dev=<int:dev_id>/severity=<int:new_severity>/priority=<int:new_priority>/', patch_update_report, name='patch_update_report'),
-    # path('defects/<int:id>/comment/', post_comment, name='post_comment'),
+    path('defects/Assigned/dev=<str:id>/', get_assigned_defects, name='get_assigned_defects'),
+    path('defect/<str:id>/', get_full_report, name='get_full_report'),
+    path('defects/<str:id>/<str:new_status>/dev=<str:dev_id>/', patch_update_report, name='patch_update_report'),
+    path('defects/<str:id>/<str:new_status>/dev=<str:dev_id>/severity=<int:new_severity>/', patch_update_report, name='patch_update_report'),
+    path('defects/<str:id>/<str:new_status>/dev=<str:dev_id>/priority=<int:new_priority>/', patch_update_report, name='patch_update_report'),
+    path('defects/<str:id>/<str:new_status>/dev=<str:dev_id>/severity=<int:new_severity>/priority=<int:new_priority>/', patch_update_report, name='patch_update_report'),
+    # path('defects/<str:id>/comment/', post_comment, name='post_comment'),
 ]
